0be5b363e43e572ca468c0dc1a304e334f7c5be4
Will S <ostielee16@gmail.com>
2021-10-25 17:22:24 +0200

    Reader is effective

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterV2.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterV2.java
@@ -1,5 +1,13 @@
 package com.hemebiotech.analytics;
 
-public class AnalyticsCounterV2 {
+import java.util.List;
 
+public class AnalyticsCounterV2 {
+	public static void main(String args[]) throws Exception {
+
+		ISymptomReader readFile = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		List<String> lines = readFile.getSymptoms();
+
+		System.out.println(lines);
+	}
 }
